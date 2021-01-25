49a0bb5b6a17f4ba029e96724cc0a77b80722356
apaterson543 <65429477+apaterson543@users.noreply.github.com>
2021-01-25 10:55:21 -0500

    Add environment variables to Tweepy.py module

--- clustering/Tweepy.py
+++ clustering/Tweepy.py
@@ -1,11 +1,12 @@
 import tweepy as tw
+import os
 
 # Fill out consumer information for Twitter API
 
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = os.environ.get("TP_CONSUMER_KEY")
+consumer_secret = os.environ.get("TP_CONSUMER_SECRET")
+access_token = os.environ.get("TP_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TP_ACCESS_TOKEN_SECRET")
 
 def get_auth():
     auth = tw.OAuthHandler(consumer_key,consumer_secret)
